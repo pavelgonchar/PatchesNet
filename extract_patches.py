1bc5a95a50207c16d8c9e9fa480a7e5386cf6d92
Andres Torrubia <antorsae@gmail.com>
2017-09-08 19:58:14 +0200

    extracts patches of different size

--- extract_patches.py
+++ extract_patches.py
@@ -6,11 +6,12 @@ import scipy.misc as misc
 import os
 from tqdm import tqdm
 import errno
+from os.path import join
+
 
 # patch size
-N = 128
+N = 256
 ROOT_DIR = '/data/pavel/carv'
-
 
 def mkdir_p(path):
     """Utility function emulating mkdir -p."""
@@ -22,8 +23,11 @@ def mkdir_p(path):
         else:
             raise
 
-mkdir_p(ROOT_DIR + '/train_patches')
-mkdir_p(ROOT_DIR + '/train_patches_masks')
+TRAIN_FOLDER_PATCHES = join(ROOT_DIR, 'train_patches_' + str(N))
+TRAIN_FOLDER_MASKS   = join(ROOT_DIR, 'train_patches_masks_' + str(N))
+
+mkdir_p(TRAIN_FOLDER_PATCHES)
+mkdir_p(TRAIN_FOLDER_MASKS)
 
 ids = [os.path.basename(x) for x in glob.glob(ROOT_DIR + '/train_hq/*.jpg')]
 ids = [x.split('.')[0] for x in ids]
@@ -49,8 +53,8 @@ for j in tqdm(range(len(ids))):
     i = 0
     for x, y in zip(np.nonzero(border)[0], np.nonzero(border)[1]):
         if i % 50 == 0 and x - N // 2 >= 0 and y - N // 2 >= 0:
-            misc.imsave(ROOT_DIR + '/train_patches/%s_%s.jpg' %
-                        (ids[j], i), img[x - N // 2:x + N // 2, y - N // 2:y + N // 2, :])
-            misc.imsave(ROOT_DIR + '/train_patches_masks/%s_%s.png' %
-                        (ids[j], i), mask[x - N // 2:x + N // 2, y - N // 2:y + N // 2] * 255)
+            misc.imsave(join(TRAIN_FOLDER_PATCHES, '%s_%s.jpg' % (ids[j], i)), 
+                img[x - N // 2:x + N // 2, y - N // 2:y + N // 2, :])
+            misc.imsave(join(TRAIN_FOLDER_MASKS, '%s_%s.png' % (ids[j], i)), 
+                mask[x - N // 2:x + N // 2, y - N // 2:y + N // 2] * 255)
         i = i + 1
