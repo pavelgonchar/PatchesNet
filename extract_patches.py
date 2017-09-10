8e7338d162958f00be350008ce9eb27b4a35c8a9
Andres Torrubia <antorsae@gmail.com>
2017-09-10 10:07:30 +0200

    extract_patches.py now builds CSV with x,y locations of extracted patch

--- extract_patches.py
+++ extract_patches.py
@@ -7,7 +7,7 @@ import os
 from tqdm import tqdm
 import errno
 from os.path import join
-
+import csv
 
 # patch size
 N = 256
@@ -25,6 +25,7 @@ def mkdir_p(path):
 
 TRAIN_FOLDER_PATCHES = join(ROOT_DIR, 'train_patches_' + str(N))
 TRAIN_FOLDER_MASKS   = join(ROOT_DIR, 'train_patches_masks_' + str(N))
+CSVFILENAME          = join(ROOT_DIR, 'train_patches_' + str(N) + ".csv")
 
 mkdir_p(TRAIN_FOLDER_PATCHES)
 mkdir_p(TRAIN_FOLDER_MASKS)
@@ -33,28 +34,32 @@ ids = [os.path.basename(x) for x in glob.glob(ROOT_DIR + '/train_hq/*.jpg')]
 ids = [x.split('.')[0] for x in ids]
 ids.sort()
 
-for j in tqdm(range(len(ids))):
-    mask = misc.imread(ROOT_DIR + '/train_masks/%s_mask.gif' %
-                      ids[j], cv2.IMREAD_GRAYSCALE)[...,0] / 255.0
-    mask = np.pad(mask, ((N // 2, N // 2), (N // 2, N // 2)), 'symmetric')
+with open(CSVFILENAME, 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for j in tqdm(range(len(ids))):
+        mask = misc.imread(ROOT_DIR + '/train_masks/%s_mask.gif' %
+                          ids[j], cv2.IMREAD_GRAYSCALE)[...,0] / 255.0
+        mask = np.pad(mask, ((N // 2, N // 2), (N // 2, N // 2)), 'symmetric')
 
-    border = np.abs(np.gradient(mask)[1]) + np.abs(np.gradient(mask)[0])
-    border = np.select([border == 0.5, border != 0.5], [1.0, border])
+        border = np.abs(np.gradient(mask)[1]) + np.abs(np.gradient(mask)[0])
+        border = np.select([border == 0.5, border != 0.5], [1.0, border])
 
-    img = cv2.imread(ROOT_DIR + '/train_hq/%s.jpg' % ids[j])
-    img = np.pad(
-        img, ((N // 2, N // 2), (N // 2, N // 2), (0, 0)), 'symmetric')
+        img = cv2.imread(ROOT_DIR + '/train_hq/%s.jpg' % ids[j])
+        img = np.pad(
+            img, ((N // 2, N // 2), (N // 2, N // 2), (0, 0)), 'symmetric')
 
-    height, width = mask.shape
+        height, width = mask.shape
 
-    patches_img = []
-    patches_mask = []
+        patches_img = []
+        patches_mask = []
 
-    i = 0
-    for x, y in zip(np.nonzero(border)[0], np.nonzero(border)[1]):
-        if i % 50 == 0 and x - N // 2 >= 0 and y - N // 2 >= 0 and x + N // 2 < img.shape[0] and y + N // 2 < img.shape[1]:
-            misc.imsave(join(TRAIN_FOLDER_PATCHES, '%s_%s.jpg' % (ids[j], i)), 
-                img[x - N // 2:x + N // 2, y - N // 2:y + N // 2, :])
-            misc.imsave(join(TRAIN_FOLDER_MASKS, '%s_%s.png' % (ids[j], i)), 
-                mask[x - N // 2:x + N // 2, y - N // 2:y + N // 2] * 255)
-        i = i + 1
+        i = 0
+        for x, y in zip(np.nonzero(border)[0], np.nonzero(border)[1]):
+            if i % 50 == 0 and x - N // 2 >= 0 and y - N // 2 >= 0 and x + N // 2 < img.shape[0] and y + N // 2 < img.shape[1]:
+                patch_filename = '%s_%s.jpg' % (ids[j], i)
+                misc.imsave(join(TRAIN_FOLDER_PATCHES, patch_filename), 
+                    img[x - N // 2:x + N // 2, y - N // 2:y + N // 2, :])
+                misc.imsave(join(TRAIN_FOLDER_MASKS, patch_filename), 
+                    mask[x - N // 2:x + N // 2, y - N // 2:y + N // 2] * 255)
+                writer.writerow([patch_filename, y, x])
+            i = i + 1
